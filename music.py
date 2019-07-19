ec9d1db544a6c27cb23a97cb08599a35c74b455b
Royce <rrysato@gmail.com>
2019-07-19 00:40:34 -0700

    can't seem to find a good lib to use lol

--- music.py
+++ music.py
@@ -1,39 +1,22 @@
-from pydub import AudioSegment as AS
-import pydub.playback as pb
+import playsound
 
 import keys
 
 
 def init_music():
-    
-    pgm.music.set_volume(keys.DEFAULT_VOLUME / 100);
+    return;
 
 def play(data):
-    if pgm.music.get_busy():
-        pgm.music.stop();
-    pgm.music.load(data[2]);
-    pgm.music.play();
+    return;
 
 def play_pause(disp, curs):
-    if pgm.music.get_busy():
-        pgm.music.pause();
-    else:
-        pgm.music.unpause();
+    return;
 
 def vol_up(disp, curs):
-    cur = pgm.music.get_volume();
-    new = cur + keys.VOL_STEP / 100;
-    if new > 1:
-        new = 1;
-    pgm.music.set_volume(new);
+    return;
         
 def vol_down(disp, curs):
-    cur = pgm.music.get_volume();
-    new = cur - keys.VOL_STEP / 100;
-    if new < 0:
-        new = 0;
-    pgm.music.set_volume(new);
-
+    return;
                                                                                                                     
     
 
