126540fad366d5828b8b28795e116473a65f822b
Royce <rrysato@gmail.com>
2019-07-25 00:52:07 -0700

    try to use python-mpv

--- music.py
+++ music.py
@@ -4,31 +4,29 @@ import atexit
 import json
 import subprocess as sp
 import threading
+import mpv
 import os
 
 import keys
 
 global mpv_proc
 
-class MPV:
-    def __init__(self, pipe):
-        self.pipe = pipe;
-        self.p = sp.Popen(['mpv', '--idle', '--no-video', '--input-ipc-server='+pipe], stdout=sp.DEVNULL, stderr = sp.DEVNULL);
+class my_mpv:
+    def __init__(self):
+        self.mpv = mpv.MPV(__no_video=True);
         self.vol = keys.DEFAULT_VOLUME;
+        @self.mpv.property_observer()
         self.set_prop(['"volume"', str(self.vol)]);
 
         self.thread = threading.Thread(target = self.checkq);
-        self.thread.start();
 
     def checkq(self):
-
-        while True:
-            p=sp.Popen(['socat ' + self.pipe + ' -'], shell=True, stdout=sp.PIPE);
-            ap = p.communicate();
-            if not ap:
-                event = json.loads(ap[0]);
-                if event['event'] == 'end-file':
-                    sys.exit();
+        p=sp.Popen(['socat ' + self.pipe + ' -'], shell=True, stdout=sp.PIPE);
+        ap = p.communicate();
+        if not ap:
+            event = json.loads(ap[0]);
+            if event['event'] == 'end-file':
+                sys.exit();
             
 
     def ex(self, args):
@@ -64,7 +62,8 @@ class MPV:
     def play(self, *args):
         self.ex(['"loadfile"', '"' + args[0][2] + '"']);
         self.set_prop(['"pause"', 'false']);
-
+        self.thread.start();
+        
     def play_pause(self, *args):
         a = self.get_prop('"pause"');
         out = json.loads(a[0]);
@@ -84,11 +83,6 @@ class MPV:
         self.p.kill()
 
 def init_music():
-    mpv_proc = MPV(keys.PIPE);
+    mpv_proc = my_mpv(__no_video=True);
     atexit.register(mpv_proc.__del__);
     return mpv_proc;
-
-def play(data):
-    global mpv_proc
-    mpv_proc.ex(['"loadfile"', '"' + data[2] + '"']);
-    return;
